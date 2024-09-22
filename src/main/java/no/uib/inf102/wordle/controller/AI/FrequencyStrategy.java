ad235751b2f1d84d5c0ecc72d400e6f0b23679eb
Johanne Blikberg Herheim <joher2128@uib.no>
2024-09-22 13:15:33 +0200

    Add FrequencyStrategy

--- src/main/java/no/uib/inf102/wordle/controller/AI/FrequencyStrategy.java
+++ src/main/java/no/uib/inf102/wordle/controller/AI/FrequencyStrategy.java
@@ -1,13 +1,16 @@
 package no.uib.inf102.wordle.controller.AI;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import no.uib.inf102.wordle.model.Dictionary;
 import no.uib.inf102.wordle.model.word.WordleWord;
 import no.uib.inf102.wordle.model.word.WordleWordList;
 
 /**
  * This strategy finds the word within the possible words which has the highest
- * expected
- * number of green matches.
+ * expected number of green matches.
  */
 public class FrequencyStrategy implements IStrategy {
 
@@ -21,10 +24,41 @@ public class FrequencyStrategy implements IStrategy {
 
     @Override
     public String makeGuess(WordleWord feedback) {
-        // TODO: Implement me :)
-        return "";
+        HashMap<Character, Integer> frequencyMap = getMostUsedLetters(dictionary);
+        String bestGuess = findBestGuess(frequencyMap, dictionary.getGuessWordsList());
+        return bestGuess;
     }
 
+    private String findBestGuess(HashMap<Character, Integer> freqMap, List<String> wordlist) {
+
+        if (wordlist.isEmpty())
+            return "";
+
+        String bestWord = wordlist.get(0);
+        int bestScore = Integer.MIN_VALUE;
+
+        for (String s : wordlist) {
+            int tempScore = 0;
+            for (Character c : s.toCharArray()) {
+                tempScore += freqMap.getOrDefault(c, 0);
+            }
+            if (tempScore > bestScore) {
+                bestScore = tempScore;
+                bestWord = s;
+            }
+        }
+        return bestWord;
+    }
+
+    private HashMap<Character, Integer> getMostUsedLetters(Dictionary dictionary) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (String s : dictionary.getAnswerWordsList()) {
+            for (Character c : s.toCharArray()) {
+                freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+            }
+        }
+        return freqMap;
+    }
 
     @Override
     public void reset() {
